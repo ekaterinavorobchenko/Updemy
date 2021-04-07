1a0c3d2f794fc9bd5bafce8247a1dae6fd23963a
ekaterinavorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-04-07 16:32:57 +0300

    APPIUM first program

--- src/test/java/base.java
+++ src/test/java/base.java
@@ -1,13 +1,27 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.HttpCommandExecutor;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class base {
-    public static void main(String[] arg) {
+    public static void main(String[] arg) throws MalformedURLException {
+
+        File f=new File("scr");
+        File fs=new File(f,"ApiDemos-debug.apk");
 
         DesiredCapabilities cap=new DesiredCapabilities();
         cap.setCapability(MobileCapabilityType.DEVICE_NAME,"ce0516053d11143b02");
-        cap.setCapability(MobileCapabilityType.APP,"/Users/ekaterinavolobchenko/Downloads/demo.apk" );
+        cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
+        AndroidDriver<AndroidElement> driver=new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+        //AndroidUIAutomator
+
+
 
     }
 }
