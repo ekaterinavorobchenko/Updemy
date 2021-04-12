ab60afca2d718c1cb17444f18ac8b56209b36c72
ekaterinavorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-04-12 10:06:01 +0300

    Touch Actions Sequence Execution

--- src/test/java/Gestures/Gestures.java
+++ src/test/java/Gestures/Gestures.java
@@ -3,13 +3,18 @@ package Gestures;
 import base.Base;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.LongPressOptions;
+import io.appium.java_client.touch.offset.ElementOption;
 import org.openqa.selenium.WebElement;
-import static io.appium.java_client.touch.TapOptions.tapOptions;
-import static io.appium.java_client.touch.offset.ElementOption.element;
-
 
 import java.net.MalformedURLException;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
+
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+import static java.time.Duration.ofSeconds;
 
 public class Gestures extends Base {
     public static void main(String[] arg) throws MalformedURLException{
@@ -17,9 +22,16 @@ public class Gestures extends Base {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Views\"]").click();
         //Tap
+
         TouchAction t = new TouchAction(driver);
         WebElement expandList = driver.findElementByXPath("\t\n" +
                 "//android.widget.TextView[@content-desc=\"Expandable Lists\"]");
         t.tap(tapOptions().withElement(element(expandList))).perform();
+        driver.findElementByXPath("//android.widget.TextView[@content-desc=\"1. Custom Adapter\"]").click();
+
+        WebElement pn = driver.findElementByXPath("\t\n" +
+                "/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ExpandableListView/android.widget.TextView[1]");
+        t.longPress(LongPressOptions.longPressOptions().withElement(ElementOption.element(pn)).withDuration(Duration.ofSeconds(2))).release().perform();
+        System.out.println(driver.findElementById("android:id/title").isDisplayed());
     }
 }
