6a3fe17794cf986d5fa26d804992ced11667fed1
ekaterinavorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-04-13 14:31:35 +0300

    Validating the orders in checkout page

--- src/test/java/Ecommerce_tc_3/Ecommerce_tc_3.java
+++ src/test/java/Ecommerce_tc_3/Ecommerce_tc_3.java
@@ -5,15 +5,18 @@ import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
 import java.net.MalformedURLException;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class Ecommerce_tc_3 extends Base {
     public static void main(String[] arg) throws MalformedURLException, InterruptedException {
         AndroidDriver driver = capabilities();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
         driver.findElementById("com.androidsample.generalstore:id/nameField").sendKeys("Hello");
         driver.hideKeyboard();
         driver.findElementByXPath("//*[@text='Female']").click();
@@ -21,16 +24,16 @@ public class Ecommerce_tc_3 extends Base {
         driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
         driver.findElementByXPath("//*[@text='Argentina']").click();
         driver.findElementById("com.androidsample.generalstore:id/btnLetsShop").click();
+        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().resourceId(\"com.androidsample.generalstore:id/rvProductList\")).scrollIntoView(new UiSelector().textMatches(\"Nike Blazer Mid '77\").instance(0))");
 
-        driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().resourceId" +
-                "(\"com.androidsample.generalstore:id/rvProductList\")).scrollIntoView(new UiSelector().textMatches" +
-                "(\"Jordan 6 Rings\").instance(0))"));
-        driver.findElement(By.id("com.androidsample.generalstore:id/productAddCart")).click();
+        AndroidElement element_1 = (AndroidElement) driver.findElements(By.id("com.androidsample.generalstore:id/productAddCart")).get(0);
+        element_1.click();
+        AndroidElement element_2 = (AndroidElement) driver.findElements(By.id("com.androidsample.generalstore:id/productAddCart")).get(1);
+        element_2.click();
+
         driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
-        Thread.sleep(5000);
-        String lastpageText = driver.findElementById("com.androidsample.generalstore:id/productName").getAttribute("name");
-
-        Assert.assertEquals("Jordan 6 Rings", lastpageText);
-//Thread.sleep(1000);
+        Thread.sleep(4000);
     }
 }
+
+
