69c1cf04e21adff63990c8cc89f4aa30857a34b9
ekaterinavorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-04-13 09:27:28 +0300

    Negative test for the login form

--- src/test/java/Ecommerce_tc_2/Ecommerce_tc_2.java
+++ src/test/java/Ecommerce_tc_2/Ecommerce_tc_2.java
@@ -1,17 +1,18 @@
-package Ecommerce_tc_1;
+package Ecommerce_tc_2;
 
 import base.Base;
 import io.appium.java_client.android.AndroidDriver;
+import org.testng.Assert;
 
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
-public class Ecommerce_tc_1 extends Base {
+public class Ecommerce_tc_2 extends Base {
     public static void main(String[] arg) throws MalformedURLException{
         AndroidDriver driver = capabilities();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElementById("com.androidsample.generalstore:id/nameField").sendKeys("Hello");
-        driver.hideKeyboard();
+        //driver.findElementById("com.androidsample.generalstore:id/nameField").sendKeys("Hello");
+       // driver.hideKeyboard();
         driver.findElementByXPath("//*[@text='Female']").click();
         driver.findElementById("android:id/text1").click();
         driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
@@ -19,6 +20,7 @@ public class Ecommerce_tc_1 extends Base {
         driver.findElementById("com.androidsample.generalstore:id/btnLetsShop").click();
 
         String toastMessage = driver.findElementByXPath("//android.widget.Toast[1]").getAttribute("name");
+        Assert.assertEquals("Please enter you name", toastMessage);
         System.out.println(toastMessage);
     }
 }
