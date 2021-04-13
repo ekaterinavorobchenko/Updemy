1c8705a21f4345915d2c16fc715cfb150f47d36b
ekaterinavorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-04-13 15:46:04 +0300

    Code optimization with user defined functions

--- src/test/java/Ecommerce_tc_3/Ecommerce_tc_3.java
+++ src/test/java/Ecommerce_tc_3/Ecommerce_tc_3.java
@@ -1,19 +1,18 @@
 package Ecommerce_tc_3;
 
 import base.Base;
-import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
 import java.net.MalformedURLException;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-public class Ecommerce_tc_3 extends Base {
-    public static void main(String[] arg) throws MalformedURLException, InterruptedException {
+public class Ecommerce_tc_3 extends Base
+{
+    public static void main(String[] arg) throws MalformedURLException, InterruptedException
+    {
         AndroidDriver driver = capabilities();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
@@ -33,25 +32,30 @@ public class Ecommerce_tc_3 extends Base {
         driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
         Thread.sleep(4000);
 
-        AndroidElement price_1 = (AndroidElement) driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(0);
-        String amount1 = price_1.getText();
-        amount1 = amount1.substring(1);
-        double amount1value = Double.parseDouble(amount1);
+        int count = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).size();
+        double sum = 0;
+        for (int i=0;i<count;i++)
+        {
+            AndroidElement price = (AndroidElement) driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(i);
+            String amount1 = price.getText();
+            double amount = getAmount(amount1);
+            sum = (int) (sum+amount);
 
-        AndroidElement price_2 = (AndroidElement) driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(1);
-        String amount2 = price_2.getText();
-        amount2 = amount2.substring(1);
-        double amount2value = Double.parseDouble(amount2);
-
-        double sumOfProducts = amount1value+amount2value;
-        System.out.println(sumOfProducts+"sum of products");
+        }
+        System.out.println(sum+"sum of products");
 
         String total = driver.findElement(By.id("com.androidsample.generalstore:id/totalAmountLbl")).getText();
         total = total.substring(1);
         double totalValue = Double.parseDouble(total);
         System.out.println(totalValue+"Total value");
 
-        Assert.assertEquals(sumOfProducts,totalValue);
+        Assert.assertEquals(sum,totalValue);
+    }
+    public static double getAmount(String value)
+    {
+        value = value.substring(1);
+        double amount2value = Double.parseDouble(value);
+        return amount2value;
     }
 }
 
