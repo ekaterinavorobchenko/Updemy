5d0cbef0b9cc989baaa8c4fd6dc806cc32b585ef
ekaterinavorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-04-20 17:35:24 +0300

    Page Object

--- src/test/java/PageObjects/HomePage.java
+++ src/test/java/PageObjects/HomePage.java
@@ -1,10 +1,18 @@
 package PageObjects;
 
+import basic.Basics;
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
-public class HomePage {
+public class HomePage extends Basics {
 
+    public HomePage(AppiumDriver driver)
+    {
+        PageFactory.initElements(new AppiumFieldDecorator(driver),this);
+    }
     @AndroidFindBy(xpath = "//android.widget.TextView[@text='Preference']")
     public WebElement Preferences;
     //findElementByXpath("//android.widget.TextView[@text='Preference']")
--- src/test/java/basic/Basics.java
+++ src/test/java/basic/Basics.java
@@ -1,5 +1,8 @@
 package basic;
+import PageObjects.HomePage;
+import PageObjects.Preferences;
 import base.Base;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
@@ -7,19 +10,24 @@ import java.util.concurrent.TimeUnit;
 
 public class Basics extends Base {
 
-    public static void main(String[] arg) throws MalformedURLException{
+    public static void main(String[] arg) throws MalformedURLException
+    {
 
         AndroidDriver driver = capabilities();
-
         driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-        driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+
+        HomePage h = new HomePage(driver);
+        h.Preferences.click();
+
+        Preferences p = new Preferences(driver);
+        p.Dependencies.click();
+
         driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
         driver.findElementById("android:id/checkbox").click();
         driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
         driver.findElementByClassName("android.widget.EditText").sendKeys("hello");
-        driver.findElementByXPath("\t\n" +
-                "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/" +
-                "android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.Button[2]").click();
+
+        p.buttons.get(1).click();
 
         driver.quit();
     }
